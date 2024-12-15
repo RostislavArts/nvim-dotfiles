fe490cc486777cecb85c07a935f2debc547a8391
RostislavArts <rostislavarts@gmail.com>
2024-12-15 14:53:45 +0300

    deleted pyright lsp

--- init.lua
+++ init.lua
@@ -137,12 +137,11 @@ require'nvim-treesitter.configs'.setup {
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 require("mason").setup()
 require("mason-lspconfig").setup({
-    ensure_installed = {"clangd", "gopls", "jdtls", "pyright"},
+    ensure_installed = {"clangd", "gopls", "jdtls"},
 })
 require("lspconfig").gopls.setup({capabilities = capabilities})
 require("lspconfig").clangd.setup({capabilities = capabilities})
 require("lspconfig").jdtls.setup({capabilities = capabilities})
-require("lspconfig").pyright.setup({capabilities = capabilities})
 
 local cmp = require("cmp")
 cmp.setup({
